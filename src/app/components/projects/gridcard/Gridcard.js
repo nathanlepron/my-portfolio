610959b7238e2b94ef169014227e4f3df499101b
Nathan Lepron <48675531+nathanlepron@users.noreply.github.com>
2024-04-11 09:20:01 +0200

    Init project

---
+++ src/app/components/projects/gridcard/Gridcard.js
@@ -0,0 +1,12 @@
+
+import { Grid } from "@mui/material";
+
+function Gridcard() {
+    return (
+        <Grid item xs={4}>
+            <div className="test">Test</div>
+        </Grid>
+    );
+  }
+
+export default Gridcard;
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
-import { Inter } from "next/font/google";
+import '@fontsource-variable/montserrat';
+import './assets/css/fonts.css';
 import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Container } from '@mui/material';
 
 export const metadata = {
   title: "Create Next App",
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body><Container>{children}</Container></body>
     </html>
   );
 }
